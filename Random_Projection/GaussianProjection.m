a89ce4579079c166cc39b05cbc2ff0cc091c68b1
CORNELL\dq48 <qi_dong_ping@126.com>
2017-11-21 12:48:05 -0500

    First commit
    Gaussian Projection and a main test file.
    
    Dongping Qi

---
+++ Random_Projection/GaussianProjection.m
@@ -0,0 +1,13 @@
+function [C] = GaussianProjection(A, s)
+% This is the Gaussian Projection algorithm
+% ======================================
+% Input: A: matrix to be sketched
+%        s: S = G/sqrt(s), S is n by s
+%           G is sampled i.i.d. from N(0,1)
+% ======================================
+
+n = size(A,2);
+S = randn(n, s)/sqrt(s);
+C = A*S;
+
+end
